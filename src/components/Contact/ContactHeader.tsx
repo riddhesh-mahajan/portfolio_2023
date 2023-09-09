97acf376b0f267e93e646912cd422d540863bd4d
riddhesh-mahajan <riddhesh.maha@gmail.com>
2023-09-10 00:25:48 +0530

    added contact me form

---
+++ src/components/Contact/ContactHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ContactHeader() {
+  return (
+    <div className="mb-8 ms-4">
+      <p className="mb-2 text-3xl text-slate-300">Contact</p>
+      <p className="text-slate-400">
+        Say hello, and let's begin a new chapter of connection and coll
+        aboration. We can't wait to hear from you!
+      </p>
+    </div>
+  );
+}
+
+export default ContactHeader;
